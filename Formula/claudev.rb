7a00760626f4c8a045dc5fba00fd07286f3fdedf
Maksim Chugunov <shadeoflance@gmail.com>
2026-05-13 16:04:40 +0300

    formula: claudev head-only

---
+++ Formula/claudev.rb
@@ -0,0 +1,13 @@
+class Claudev < Formula
+  desc "Pool OAuth wrapper around the claude CLI"
+  homepage "https://github.com/makscee/claudev"
+  head "https://github.com/makscee/claudev.git", branch: "main"
+
+  def install
+    bin.install "claudev"
+  end
+
+  test do
+    assert_predicate bin/"claudev", :executable?
+  end
+end
